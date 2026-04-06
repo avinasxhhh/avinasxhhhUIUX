1b34643d9a06ee8e454005bac434550b81ff61b2
Avinash Mundhe <98465067+avinasxhhh@users.noreply.github.com>
2026-04-06 17:18:32 +0530

    uncomment one line

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function loco() {
     gsap.registerPlugin(ScrollTrigger);
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
     const locoScroll = new LocomotiveScroll({
         el: document.querySelector(".main"),
